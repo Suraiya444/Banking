b173a836cce6be7a7ef4051df96584414f4caef5
Nurkhankazi <WDPF@nh.ctg>
2024-11-02 11:56:04 +0600

    add account no

--- app/Http/Controllers/Api/CustomerAccountController.php
+++ app/Http/Controllers/Api/CustomerAccountController.php
@@ -9,15 +9,21 @@ use App\Http\Controllers\Api\BaseController;
 
 class CustomerAccountController extends BaseController
 {
-    public function index()
+    public function index(Request $r)
     {
-        $data=CustomerAccount::with('customer','account_type')->get();
+        $data=CustomerAccount::with('customer','account_type');
+        if($r->customer_id){
+            $data=$data->where('customer_id',$r->customer_id);
+        }
+        $data=$data->get();
         return $this->sendResponse($data,"Customer Account List");
     }
 
     public function store(Request $request)
     {
-        $data=CustomerAccount::create($request->all());
+        $input=$request->all();
+        $input['account_no']=$input['account_type_id'] . str_pad(CustomerAccount::where('account_type_id',$input['account_type_id'])->latest()->first()?->id + 1 ?? 1,10,"0",STR_PAD_LEFT);
+        $data=CustomerAccount::create($input);
         return $this->sendResponse($data,"Customer Account created successfully");
     }
 
@@ -26,9 +32,8 @@ class CustomerAccountController extends BaseController
         return $this->sendResponse($customer_account,"Customer  Account Data");
     }
 
-    public function update(Request $request,CustomerAccount $id)
+    public function update(Request $request,$id)
     {
-        $id= $customer_account->id;
         $input =$request->all();
         unset($input['_method']);
         $data=CustomerAccount::where('id',$id)->update($input);
