8d16625cbd9e9b98b11553dd8580075a1d393796
suraiya444 <suraiya444444@gmail.com>
2024-12-18 19:31:34 +0600

    loan table edit

--- database/migrations/2024_10_12_035006_create_loans_table.php
+++ database/migrations/2024_10_12_035006_create_loans_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('loan_type_id')->constrained();
             $table->foreignId('customer_id')->constrained();
             $table->foreignId('customer_account_id')->constrained();
-            $table->foreignId('customer_type_id')->constrained();
+            $table->integer('customer_type_id')->nullable();
             $table->date('start_date');
             $table->integer('term_length');
             $table->decimal('interest_rate',10,2);
